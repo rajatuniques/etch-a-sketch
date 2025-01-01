587bd5d84c63b548996d94b71ddd687348d748bf
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2025-01-01 22:27:46 +0530

    Fix code for generating 15 cells

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const cell = document.querySelector(".cell");
 
-for(let i=0; i<16; i++) {
+for(let i=1; i<16; i++) {
     const childCell = document.createElement("div");
     childCell.classList.add("cell");
     container.appendChild(childCell);
