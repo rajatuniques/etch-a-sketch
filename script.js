322e39cf698558425c6a82ad5adafc67eaf0173e
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2025-01-03 23:28:46 +0530

    Fix adjustGrid() function

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const container = document.querySelector(".container");
 const cell = document.querySelector(".cell");
 
-for(let i=1; i<16; i++) {
-    const childCell = document.createElement("div");
-    childCell.classList.add("cell");
-    container.appendChild(childCell);
+function adjustGrid(n) {
+    let size_calc = 100/n;
+    cell.style.height = `${size_calc}%`;
+    cell.style.width = `${size_calc}%`;
+    for(let i=1; i<n*n; i++) {
+        const childCell = document.createElement("div");
+        childCell.classList.add("cell");
+        childCell.style.height = `${size_calc}%`;
+        childCell.style.width = `${size_calc}%`;
+        container.appendChild(childCell);
+    }
 }
+
+adjustGrid(100);
 
 const cells = document.querySelectorAll(".cell");
 cells.forEach((cell) => {
